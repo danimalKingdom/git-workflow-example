396f083e6e40bbd4c0220eedc1b62d28520aa207
andrew <danteadar@gmail.com>
2017-03-12 10:26:38 -0700

    created taylor controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'taylor/index'
+
+  get 'taylor/action'
+
+  get 'taylor/view'
+
   get "/" => "pages#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
